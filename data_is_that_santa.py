aa8d0faaa3e8eeff16e23214b3e5817247a42b5d
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-12-31 12:40:09 +0300

    Pred function very small upd
    Nah, I'm serious it's small

--- data_is_that_santa.py
+++ data_is_that_santa.py
@@ -57,4 +57,4 @@ def pred(im_path):
     img = tf.image.resize(img, (224, 224))
     img = img / 255.0
     img = tf.reshape(img, (1, 224, 224, 3))
-    return f"Model is {model.predict(img)[0][1] * 100}% sure it's santa {add_understanding(model.predict(img)[0][1] * 100)}. Happy new Year!"
+    return f"Model is {str(model.predict(img)[0][1] * 100)[:4]}% sure it's santa \n{add_understanding(model.predict(img)[0][1] * 100)}. \nHappy new Year!"
